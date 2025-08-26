92cba9a6da465cb8338108ea73fa8a6b5d0d5a6b
Mostafa kamel <mmohamed@nadeer.com.sa>
2025-08-26 18:15:06 +0300

    refact style

--- src/app/components/campaign/campaign-detail/champions/champions.component.ts
+++ src/app/components/campaign/campaign-detail/champions/champions.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { InputComponent } from "../../../../core/components/input/input.component";
 import { PrimaryButtonComponent } from "../../../../core/components/primary-button/primary-button.component";
 import { CampaignService } from '../../../../core/services/campaign.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-champions',
   standalone: true,
-  imports: [InputComponent, PrimaryButtonComponent],
+  imports: [InputComponent, PrimaryButtonComponent, CommonModule],
   templateUrl: './champions.component.html',
   styleUrl: './champions.component.scss'
 })
